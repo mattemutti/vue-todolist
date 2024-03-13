a6313bc6b2b19356bc5f3e5a268db8c0eaad9b03
mattemutti <mattemutti@gmail.com>
2024-03-13 15:12:37 +0100

    add task list in html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,63 @@
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    }
-  }).mount('#app')
+createApp({
+	data() {
+		return {
+			list: [
+				{
+					text: 'Quarterly Newsletter',
+					done: false,
+				},
+				{
+					text: 'Recruiting Blog Post',
+					done: false,
+				},
+				{
+					text: 'mobile app luaunch',
+					done: false,
+				},
+				{
+					text: 'Interview John H.',
+					done: false,
+				},
+				{
+					text: 'Summit update to mobile storefonts',
+					done: false,
+				},
+				{
+					text: 'Schedule Meeting with Alex',
+					done: false,
+				},
+				{
+					text: 'Hompage refresh',
+					done: false,
+				},
+				{
+					text: 'On board new sales team members',
+					done: false,
+				},
+				{
+					text: 'Review editorial calendar',
+					done: false,
+				},
+			],
+
+			//Qui andranno le funzioni
+			methods: {
+
+
+
+			},
+
+			//qui nell' hook mounted
+			mounted() {
+				//console.log(this.list),
+				//console.log("ciao")
+			}
+
+
+
+
+		}
+	}
+}).mount('#app')
--- readme.md
+++ readme.md
@@ -5,7 +5,8 @@ Rifare l'esercizio della to do list. Questa volta però ogni todo sarà un ogget
 - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
 # MILESTONE 1
-Stampare all'interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+- Stampare all'interno di una lista HTML un item per ogni todo. 
+- Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
 ## MILESTONE 2
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
